e13fab390656e0716fd05203a27e77f623d04eb5
Arnault Beroni <arnoldberonik@gmail.com>
2025-04-25 21:17:55 +0100

    resolution du probleme de cors

--- src/main/java/com/sji/group9/pointInteret/api/AddressApi.java
+++ src/main/java/com/sji/group9/pointInteret/api/AddressApi.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/api/pt-interet/address")
 public class AddressApi {
     @Autowired
--- src/main/java/com/sji/group9/pointInteret/api/PointInteretApi.java
+++ src/main/java/com/sji/group9/pointInteret/api/PointInteretApi.java
@@ -14,6 +14,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/api/pt-interet")
 public class PointInteretApi {
     @Autowired
